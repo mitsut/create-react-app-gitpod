b9cfc24c75e53c8a750fb33dd67cb2d022a7e547
mitsut <mitsu48@gmail.com>
2021-06-05 07:31:09 +0000

    Modify to embedded board.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [led1, setLed1] = useState(false);
+  const [led2, setLed2] = useState(false);
+  const [led3, setLed3] = useState(false);
+  const [led4, setLed4] = useState(false);
+  const toggle1 = () => setLed1(!led1)
+  const toggle2 = () => setLed2(!led2)
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="powerled" />
+        <img src="https://hokutodenshi.co.jp/images/photo/photo_HSBRH850F1L.jpg" className="App-board"/>
+        <button  className="psw one" onClick={toggle1}></button >
+        <button  className="psw two" onClick={toggle2}></button >
+        <div className={led1 ? "none":"led one"}/>
+        <div className={led2 ? "none":"led two"}/>
+        <div className={led3 ? "none":"led three"}/>
+        <div className={led4 ? "none":"led four"}/>
       </header>
     </div>
   );
